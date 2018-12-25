e751ee938472705df65e0d5441ce153fbb9f57bb
Stud2 <ser@adsf.ri>
2018-12-25 20:43:44 +0300

    fix problem

--- app/src/main/java/com/bic/notificator/MainActivity.java
+++ app/src/main/java/com/bic/notificator/MainActivity.java
@@ -153,45 +153,6 @@ public class MainActivity extends AppCompatActivity {
 
     ;
     // ---------------Copying from Google
-
-    public ArrayList<SMSData> getAllSms(Context context) {
-        ArrayList<SMSData> smsList;
-        smsList = new ArrayList<SMSData>();
-        ContentResolver cr = context.getContentResolver();
-        int totalSMS = 0;
-        Cursor c = cr.query(Telephony.Sms.CONTENT_URI, null, null, null, null);
-        if (c != null) {
-            totalSMS = c.getCount();
-            if (c.moveToFirst()) {
-                for (int j = 0; j < totalSMS; j++) {
-//                    String smsDate = c.getString(c.getColumnIndexOrThrow(Telephony.Sms.DATE));
-                    smsList.add(new SMSData(c.getString(c.getColumnIndexOrThrow(Telephony.Sms.ADDRESS)), c.getString(c.getColumnIndexOrThrow(Telephony.Sms.BODY))));
-//                    Date dateFormat = new Date(Long.valueOf(smsDate));
-/*                    String type;
-                    switch (Integer.parseInt(c.getString(c.getColumnIndexOrThrow(Telephony.Sms.TYPE)))) {
-                        case Telephony.Sms.MESSAGE_TYPE_INBOX:
-                            type = "inbox";
-                            break;
-                        case Telephony.Sms.MESSAGE_TYPE_SENT:
-                            type = "sent";
-                            break;
-                        case Telephony.Sms.MESSAGE_TYPE_OUTBOX:
-                            type = "outbox";
-                            break;
-                        default:
-                            break;
-                    }*/
-                    c.moveToNext();
-                }
-                c.close();
-            }
-
-        } else {
-            Toast.makeText(this, "No message to show!", Toast.LENGTH_SHORT).show();
-        }
-//        setListAdapter(new SMSListAdapter(this, smsList));
-        return smsList;
-    }
 }
 
 
